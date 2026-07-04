898e4133d94fd2dd30f1c6436f73fc2268df89d1
BL-ThamaraiKannan <thamarai.kannan@bridgelabz.com>
2026-07-04 10:46:40 +0530

    [BL-ThamaraiKannan] refactor : n-Tier Architecture Refactoring for Quantity Measurement Application

---
+++ QuantityMeasurement/com/src/main/exception/QuantityMeasurementException.java
@@ -0,0 +1,9 @@
+package com.src.main.exception;
+
+public class QuantityMeasurementException
+        extends RuntimeException {
+
+    public QuantityMeasurementException(String message) {
+        super(message);
+    }
+}
